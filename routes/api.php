97aa4ea59d1555db0967b09c9fc9c623a099e8b5
mennaahmedhosany <maneahmed20@gmail.com>
2025-12-02 22:54:02 +0200

    the payment webhook

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HoldController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PaymentWebhookController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,4 +10,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/holds', [HoldController::class, 'store']);
 Route::post('/orders', [OrderController::class, 'store']);
-Route::post('/payments/webhook', [OrderController::class, 'handlePaymentWebhook']);
+Route::post('/webhooks/payment', [PaymentWebhookController::class, 'handlePaymentWebhook']);
